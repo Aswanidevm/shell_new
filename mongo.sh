553e2bb3c324f6bd8639d456971d7a952bf8c0bd
Aswanidevm <maswanidev@gmail.com>
2023-10-31 20:50:24 +0530

    front

---
+++ mongo.sh
@@ -0,0 +1,7 @@
+dirct=$(pwd)
+cp cp $dirct/config/mongo.repo  /etc/yum.repos.d/mongo.repo
+sudo dnf install mongodb-org -y
+sudo systemctl start mongod
+sudo systemctl ebable mongod
+sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+sudo systemctl restart mongod
