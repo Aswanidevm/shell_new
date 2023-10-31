340aa002679f7ff2f51056f5d73098a19092e578
Aswanidevm <maswanidev@gmail.com>
2023-10-31 20:13:53 +0530

    front

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,11 @@
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
+sudo dnf install nginx -y
+
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
-rm -rf *
-yum install unzip
-unzip /tmp/frontend.zip
-
+sudo rm -rf *
+yum install unzip -y
+sudo unzip /tmp/frontend.zip
+systemctl enable nginx
+systemctl start nginx
 cp config/roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
