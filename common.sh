ca1c9b402da9c364e35628e264400d3fcdd9c355
Aswanidevm <maswanidev@gmail.com>
2023-11-01 14:30:32 +0530

    front

--- common.sh
+++ common.sh
@@ -1,5 +1,39 @@
 dirct=$(pwd)
+touch /tmp/log_file
+rm -rf /tmp/log_file
 
-useradd roboshop
+aftifacts_setup()
+{
+  getent passwd roboshop &>> log_file
 
-mkdir /app
+  if [ $? -eq 0 ]; then
+      echo "yes the user exists"
+  else
+      echo "No, the user does not exist"
+      useradd roboshop
+  fi
+
+  curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.
+
+  mkdir /app
+
+  cd /app
+
+  rm -rf *
+
+  unzip /tmp/dispatch.zip
+
+}
+
+golang()
+{
+dnf install golang -y
+aftifacts_setup
+go mod init dispatch
+go get
+go build
+cp $(dirct)/config/$(component).service /etc/systemd/system/$(component).service
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl start dispatch
+}
--- dispatch.sh
+++ dispatch.sh
@@ -1,14 +1,3 @@
 source common.sh
-
-dnf install golang -y
-
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-cd /app
-unzip /tmp/dispatch.zip
-go mod init dispatch
-go get
-go build
-cp $dirct/config/dispatch.service /etc/systemd/system/dispatch.service
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+component=dispatch
+golang
