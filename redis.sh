8384040c705f1f8741dc4055e33ba494769b36b9
Aswanidevm <maswanidev@gmail.com>
2023-11-01 07:56:05 +0530

    front

--- redis.sh
+++ redis.sh
@@ -2,7 +2,6 @@ dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 dnf install redis -y
 
-sudo sed -e '/s/127.0.0.1/0.0.0.0/g' /etc/redis.conf
-sudo sed -e '/s/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+sudo sed -i -e'/s/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf
 systemctl enable redis
 systemctl start redis
