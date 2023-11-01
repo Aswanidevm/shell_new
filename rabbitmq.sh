29ed7cbd7a69645ae8009e5f0cc643bacc25efe0
Aswanidevm <maswanidev@gmail.com>
2023-11-01 20:55:47 +0530

    simple

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,22 @@
+source common.sh
+
+echo " Installing erlang"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+status $?
+
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+status $?
+
+echo " Installing rabbitmq"
 dnf install rabbitmq-server -y
+status $?
+
+echo " Enabling rabittmq server "
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
+status $?
+
+echo " Adding User Roboshop "
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+status $?
