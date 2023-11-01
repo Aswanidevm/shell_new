19743e4f135e7f8f757fe6da9182a7f7e5b7d0ae
Aswanidevm <maswanidev@gmail.com>
2023-11-01 12:48:20 +0530

    front

---
+++ payment.sh
@@ -0,0 +1,12 @@
+dirct=$(pwd)
+dnf install python36 gcc python3-devel -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
+cd /app
+unzip /tmp/payment.zip
+pip3.6 install -r requirements.txt
+cp $dirct/config/payment.service /etc/systemd/system/payment.service
+systemctl daemon-reload
+systemctl enable payment
+systemctl start payment
